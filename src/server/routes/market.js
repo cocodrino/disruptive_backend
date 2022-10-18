a5cc992474c4684c6297fdafcb7583ad37ace609
carlos LM <clagccs@gmail.com>
2022-10-18 17:49:24 -0400

    top loser brings information from testnet instead of API wallet

--- src/server/routes/market.js
+++ src/server/routes/market.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {BinanceRealAccount} from "../../services/defaultInstances/Binance";
+import {BinanceTestnetAccount} from "../../services/defaultInstances/Binance";
 
 var router = express.Router();
 
@@ -9,7 +9,7 @@ router.get('/top-losers', async function(req, res) {
   const count = +(req.query.count || "10")
 
   console.info("retrieving top losers, this can take a time")
-  const  losers = await BinanceRealAccount.getTopLosers(quote,count)
+  const  losers = await BinanceTestnetAccount.getTopLosers(quote,count)
   res.json(losers)
 });
 
